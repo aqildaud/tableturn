b1b477587b35ab3deca55fde94d3de8c2b11c6d2
salmah <aqildaud611@gmail.com>
2024-10-07 23:53:55 +0800

    Initial commit

---
+++ turntable.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+void swap(char **a, char **b) {
+    char *t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void permute(char **a, int l, int r, char ***s, int *count) {
+    if (l == r) {
+        // store the current permutation in s
+        (*s) = realloc(*s, (*count + 1) * sizeof(char *));
+        (*s)[*count] = (char *)malloc(1024);  // allocate memory for the sentence
+        (*s)[*count][0] = '\0';  // initialize the string
+
+        for (int i = 0; i <= r; i++) {
+            strcat((*s)[*count], a[i]);  // append the word
+            strcat((*s)[*count], " ");
+        }
+        (*count)++;
+    } else {
+        for (int i = l; i <= r; i++) {
+            swap(&a[l], &a[i]);
+            permute(a, l + 1, r, s, count);
+            swap(&a[l], &a[i]);
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Improper input. Exit\n");
+        return 1;
+    }
+
+    char **strings = NULL;
+    const char d[] = " ";
+    char *token;
+    int current_size = 0;
+
+    token = strtok(argv[1], d);
+
+    while( token != NULL ) {
+        strings = realloc(strings, (current_size + 1) * sizeof(char *));
+        strings[current_size] = strdup(token);
+        current_size++;
+        token = strtok(NULL, d);
+    }
+
+    char **p = NULL;
+    int permutation_count = 0;
+
+    if (current_size > 0) {
+        // generate all permutations
+        permute(strings, 0, current_size - 1, &p, &permutation_count);
+    }
+    // to prevent sequence repetition between runs 
+    srand(time(NULL));
+
+    int r = rand() % permutation_count;
+
+    // print the randomly picked permutation
+    printf("%s\n", p[r]);
+
+    for (int i = 0; i < current_size; i++) {
+        free(strings[i]);
+    }
+    free(strings);
+
+    for (int i = 0; i < permutation_count; i++) {
+        free(p[i]);
+    }
+    free(p);
+
+    return 0;
+}
